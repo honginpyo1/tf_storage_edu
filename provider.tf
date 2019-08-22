5ea35c096e205e4261a572d0852da23f46b7097e
inpyo hong <honginpyo1@hanmail.net>
2019-08-22 10:16:02 +0900

    change region

--- provider.tf
+++ provider.tf
@@ -14,5 +14,5 @@ provider "ibm" {
   softlayer_username = "${var.softlayer_username}"
   softlayer_api_key  = "${var.softlayer_api_key}"
   ibmcloud_api_key   = "${var.ibmcloud_api_key}"
-  region = "kr-seo"
+  region = "jp-tok"
 }
